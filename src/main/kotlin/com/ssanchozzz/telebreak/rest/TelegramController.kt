d34921e3847b30f288b822dc7b77190cabac288a
Alexander Bogdanov <alexander@bogdanov.ru>
2021-10-09 15:36:37 +0300

    Added tests, fixed logic

--- src/main/kotlin/com/ssanchozzz/telebreak/rest/TelegramController.kt
+++ src/main/kotlin/com/ssanchozzz/telebreak/rest/TelegramController.kt
@@ -12,32 +12,32 @@ class TelegramController {
 
     private val classDurationMinutes = 45L
 
-    private val secondBuildingSchedule = listOf(
-        of(8, 30),
-        of(9, 25),
-        of(10, 30),
-        of(11, 25),
-        of(12, 30),
-        of(13, 25),
-        of(14, 20),
-        of(15, 25)
+    private val firstBuildingSchedule = listOf(
+            of(9, 0),
+            of(9, 55),
+            of(11, 0),
+            of(11, 55),
+            of(13, 55),
+            of(14, 50),
+            of(15, 45)
     )
 
-    private val firstBuildingSchedule = listOf(
-        of(9, 0),
-        of(9, 55),
-        of(11, 0),
-        of(11, 55),
-        of(13, 55),
-        of(14, 50),
-        of(15, 45)
+    private val secondBuildingSchedule = listOf(
+            of(8, 30),
+            of(9, 25),
+            of(10, 30),
+            of(11, 25),
+            of(12, 30),
+            of(13, 25),
+            of(14, 20),
+            of(15, 25)
     )
 
     private val schedules = mapOf(
-        MONDAY to secondBuildingSchedule,
-        TUESDAY to firstBuildingSchedule,
-        WEDNESDAY to secondBuildingSchedule,
-        THURSDAY to firstBuildingSchedule
+            MONDAY to secondBuildingSchedule,
+            TUESDAY to firstBuildingSchedule,
+            WEDNESDAY to secondBuildingSchedule,
+            THURSDAY to firstBuildingSchedule
     )
 
     @GetMapping("closestBreak")
@@ -53,20 +53,19 @@ class TelegramController {
         }
 
         val schedule = schedules[referenceDateTime.dayOfWeek]!!
-        val found = schedule.binarySearch(referenceDateTime.toLocalTime())
-        if (found < 0 || found == 0
-            && MINUTES.between(referenceDateTime, schedule.first()) > classDurationMinutes
-        ) {
+        val searchResult = schedule.binarySearch(referenceDateTime.toLocalTime())
+        val insertionPoint = if (searchResult >= 0) searchResult else -(searchResult + 1)
+        if (insertionPoint == 0) {
             return "Classes not yet started"
-        } else if (found == schedule.size
-            && MINUTES.between(schedule.last(), referenceDateTime) > classDurationMinutes
+        } else if (insertionPoint == schedule.size
+                && MINUTES.between(schedule.last(), referenceDateTime) > classDurationMinutes
         ) {
             return "Classes were finished for today"
         }
 
-        val previousClassStart = schedule[found - 1]
+        val previousClassStart = schedule[insertionPoint - 1]
         if (MINUTES.between(previousClassStart, referenceDateTime) < classDurationMinutes) {
-            return "Next break in ${classDurationMinutes - MINUTES.between(previousClassStart, referenceDateTime)} minutes"
+            return "Next break is in ${classDurationMinutes - MINUTES.between(previousClassStart, referenceDateTime)} minutes"
         }
 
         return "It's a break now!"
--- src/test/kotlin/com/ssanchozzz/telebreak/rest/TelegramControllerTest.kt
+++ src/test/kotlin/com/ssanchozzz/telebreak/rest/TelegramControllerTest.kt
@@ -9,10 +9,50 @@ internal class TelegramControllerTest {
     private val tc = TelegramController()
 
     @Test
-    fun test() {
+    fun testClassesNotYetStarted() {
         Assertions.assertEquals(
-            "Classes not yet started",
-            tc.getClosestBreak(of(2021, 10, 7, 8, 0))
+                "Classes not yet started",
+                tc.getClosestBreak(of(2021, 10, 7, 8, 0))
+        )
+    }
+
+    @Test
+    internal fun testNoClassesToday() {
+        Assertions.assertEquals(
+                "No classes today",
+                tc.getClosestBreak(of(2021, 10, 9, 8, 0))
+        )
+    }
+
+    @Test
+    internal fun testClassesWereFinishedForToday() {
+        Assertions.assertEquals(
+                "Classes were finished for today",
+                tc.getClosestBreak(of(2021, 10, 7, 17, 0))
+        )
+    }
+
+    @Test
+    internal fun testNextBreakFirstBuildingSchedule() {
+        Assertions.assertEquals(
+                "Next break is in 15 minutes",
+                tc.getClosestBreak(of(2021, 10, 7, 9, 30))
+        )
+    }
+
+    @Test
+    internal fun testNextBreakSecondBuildingSchedule() {
+        Assertions.assertEquals(
+                "Next break is in 40 minutes",
+                tc.getClosestBreak(of(2021, 10, 6, 9, 30))
+        )
+    }
+
+    @Test
+    internal fun testIsBreakNowSecondBuildingSchedule() {
+        Assertions.assertEquals(
+                "It's a break now!",
+                tc.getClosestBreak(of(2021, 10, 6, 9, 20))
         )
     }
 }
