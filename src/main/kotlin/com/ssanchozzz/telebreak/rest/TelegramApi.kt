4e020fdabeb1b2983a42c17bf6990fb4f5cbe052
Alexander Bogdanov <alexander@bogdanov.ru>
2021-10-17 00:59:45 +0300

    made a while loop after application ready instead of scheduling

--- src/main/kotlin/com/ssanchozzz/telebreak/rest/TelegramApi.kt
+++ src/main/kotlin/com/ssanchozzz/telebreak/rest/TelegramApi.kt
@@ -8,25 +8,33 @@ import com.ssanchozzz.telebreak.domain.UserResponse
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.event.EventListener
 import org.springframework.http.HttpMethod.GET
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.Duration
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+
 
 @Component
-@EnableScheduling
 class TelegramApi(
-        @Value("\${telegram.bot.url}") val url: String,
-        @Value("\${telegram.bot.token}") val token: String,
-        val breakCalculator: BreakCalculator
+    @Value("\${telegram.bot.url}") val url: String,
+    @Value("\${telegram.bot.token}") val token: String,
+    val breakCalculator: BreakCalculator
 ) : InitializingBean {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
-    private val restTemplate = RestTemplateBuilder().build()
+    private val restTemplate = RestTemplateBuilder()
+        .setConnectTimeout(Duration.of(60, ChronoUnit.SECONDS))
+        .setReadTimeout(Duration.of(60, ChronoUnit.SECONDS))
+        .build()
+
     private var offset: Int = 0
 
     override fun afterPropertiesSet() {
@@ -40,39 +48,43 @@ class TelegramApi(
         log.debug(user.toString())
     }
 
-    @Scheduled(fixedDelay = 1000)
-    private fun getUpdates(): Unit? = try {
-        log.info("Making a request to $url with offset $offset")
+    @EventListener(ApplicationReadyEvent::class)
+    private fun getUpdates(): Unit? {
+        while (true) {
+            try {
+                log.info("Making a request to $url with offset $offset")
 
-        val response = restTemplate.exchange(
-                "$url$token/getUpdates?offset=$offset",
-                GET,
-                null,
-                UpdateResponse::class.java
-        )
-        log.info("Got a response from $url, status code ${response.statusCode}")
+                val response = restTemplate.exchange(
+                    "$url$token/getUpdates?offset=$offset",
+                    GET,
+                    null,
+                    UpdateResponse::class.java
+                )
+                log.info("Got a response from $url, status code ${response.statusCode}")
 
-        log.debug(response.toString())
-        val result = response.body!!.result
-        result.forEach {
-            val message = it.message ?: return@forEach
+                log.debug(response.toString())
+                val result = response.body!!.result
+                result.forEach {
+                    val message = it.message ?: return@forEach
 
-            log.info("Received a message, processing: $message")
+                    log.info("Received a message, processing: $message")
 
-            val perifCommand = "/perif"
-            if (message.text == null) {
-                return@forEach
-            }
-            if (message.text.startsWith(perifCommand)) {
-                processPerifCommand(perifCommand, message)
-            } else if (message.text.startsWith("/")) {
-                log.info("Unknown command $message")
+                    val perifCommand = "/perif"
+                    if (message.text == null) {
+                        return@forEach
+                    }
+                    if (message.text.startsWith(perifCommand)) {
+                        processPerifCommand(perifCommand, message)
+                    } else if (message.text.startsWith("/")) {
+                        log.info("Unknown command $message")
+                    }
+                }
+
+                result.lastOrNull()?.let { update -> offset = update.id + 1 }
+            } catch (e: Exception) {
+                log.info("Failed to process request", e)
             }
         }
-
-        result.lastOrNull()?.let { update -> offset = update.id + 1 }
-    } catch (e: Exception) {
-        log.info("Failed to process request", e)
     }
 
     private fun processPerifCommand(perifCommand: String, message: Message) {
@@ -104,10 +116,10 @@ class TelegramApi(
     }
 
     private fun sendMessage(chatId: Int, message: String) = restTemplate.exchange(
-            "$url$token/sendMessage?chat_id=$chatId&text=$message",
-            GET,
-            null,
-            MessageResponse::class.java
+        "$url$token/sendMessage?chat_id=$chatId&text=$message",
+        GET,
+        null,
+        MessageResponse::class.java
     )
 
     private fun setWebhook() {
